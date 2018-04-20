71c1e10a90f50626abee5184d7a84691669e3c43
EmanuelBachmann <emanuel@familie-bachmann.at>
2018-04-20 15:21:36 +0200

    Update1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { Input } from '@angular/core/src/metadata/directives';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,49 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public alertCtrl: AlertController) {}
 
+  meassureArr : any = [];
+
+
+  //object : any = [];  
+  nameDescription : string;
+  distance: string;
+  //value : string = "11m";
+
+  doPrompt() 
+  {
+    
+    let prompt = this.alertCtrl.create({
+      title: "11m",
+      message: "Beschreibung:",
+      inputs: [
+        {
+          name: 'Beschreibung',
+          placeholder: 'Abstandsmessung'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            console.log('Saved clicked');
+            var meassure = {
+              nameDescription: data.Beschreibung,
+              distance: "11m"
+            }
+            this.meassureArr.push(meassure);
+            console.log(this.meassureArr);
+          }
+        }
+      ]   
+    });   
+    prompt.present();
   }
-
 }
